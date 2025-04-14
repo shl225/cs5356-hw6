74ac0353b947e5ea7e5d8ece04fbdccc4ba2392a
shl225 <shl225@cornell.edu>
2025-04-13 22:51:36 -0500

    Update todos.ts

--- actions/todos.ts
+++ actions/todos.ts
@@ -80,8 +80,7 @@ export async function deleteTodo(formData: FormData) {
 
     //checking if user is admin
     if (!session || session.user.role !== 'admin') {
-        console.error("Unauthorized: Only admins can delete todos");
-        return;
+        throw new Error("Unauthorized: Only admins can delete todos");
     }
 
     const id = formData.get("id") as string;
@@ -91,6 +90,7 @@ export async function deleteTodo(formData: FormData) {
         revalidatePath("/admin");
     } catch (error) {
         console.error("Failed to delete todo", error);
+        throw error;
     }
 
     // const id = formData.get("id") as string;
