de16b0c6ace99a7bb9acc24610580b60558abbda
shl225 <shl225@cornell.edu>
2025-04-14 18:37:35 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,29 +2,8 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
     experimental: {
-        nodeMiddleware: true,  
+        nodeMiddleware: true,
     },
-    async headers() {
-        return [
-            {
-                source: '/api/auth/(.*)',  
-                headers: [
-                    { key: 'Access-Control-Allow-Origin', value: 'https://cs5356-hw6-git-main-shl225s-projects.vercel.app' },
-                    { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
-                    { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
-                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
-                ],
-            },
-        ]
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/todos',
-                destination: '/api/todos',  
-            },
-        ]
-    }
 }
 
 export default nextConfig
